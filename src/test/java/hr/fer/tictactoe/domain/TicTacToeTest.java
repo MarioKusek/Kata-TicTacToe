70bb6a1fcf13b74a1c88a43dc71d7c0b7925fefa
MarioKusek <mario.kusek@fer.hr>
2019-03-23 14:12:00 +0100

    Grouped tests into nested class.

--- src/test/java/hr/fer/tictactoe/domain/TicTacToeTest.java
+++ src/test/java/hr/fer/tictactoe/domain/TicTacToeTest.java
@@ -6,50 +6,55 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class TicTacToeTest {
 
 	private TicTacToe game;
-	
+
 	@BeforeEach
 	void newGame() {
 		game = new TicTacToe(null);
 	}
+	
+	@Nested
+	class ChackingEnteringValidPositions {
 
-	@Test
-	void emptyFieldPositionInNewGame() throws Exception {
-		assertThat(game.getFieldValue("a1")).isEqualTo("");
-	}
-
-	@Test
-	void enterFieldPosition() throws Exception {
-		game.play("a1");
-		
-		assertThat(game.getFieldValue("a1")).isEqualTo("o");
-	}
-
-	@Test
-	void enterPositionForTwoPlayers() throws Exception {
-		game.play("a1");
-		game.play("b1");
-		
-		assertThat(game.getFieldValue("a1")).isEqualTo("o");
-		assertThat(game.getFieldValue("b1")).isEqualTo("x");
-	}
-
-	@Test
-	void enterPositionsForMultipleMoves() throws Exception {
-		game.play("a1");
-		game.play("b1");
-		game.play("a2");
-		game.play("b3");
-		
-		assertThat(game.getFieldValue("a1")).isEqualTo("o");
-		assertThat(game.getFieldValue("b1")).isEqualTo("x");
-		assertThat(game.getFieldValue("a2")).isEqualTo("o");
-		assertThat(game.getFieldValue("b3")).isEqualTo("x");
+		@Test
+		void emptyFieldPositionInNewGame() throws Exception {
+			assertThat(game.getFieldValue("a1")).isEqualTo("");
+		}
+	
+		@Test
+		void enterFieldPosition() throws Exception {
+			game.play("a1");
+			
+			assertThat(game.getFieldValue("a1")).isEqualTo("o");
+		}
+	
+		@Test
+		void enterPositionForTwoPlayers() throws Exception {
+			game.play("a1");
+			game.play("b1");
+			
+			assertThat(game.getFieldValue("a1")).isEqualTo("o");
+			assertThat(game.getFieldValue("b1")).isEqualTo("x");
+		}
+	
+		@Test
+		void enterPositionsForMultipleMoves() throws Exception {
+			game.play("a1");
+			game.play("b1");
+			game.play("a2");
+			game.play("b3");
+			
+			assertThat(game.getFieldValue("a1")).isEqualTo("o");
+			assertThat(game.getFieldValue("b1")).isEqualTo("x");
+			assertThat(game.getFieldValue("a2")).isEqualTo("o");
+			assertThat(game.getFieldValue("b3")).isEqualTo("x");
+		}
 	}
 	
 	@Test

63c852f87b8c788f5689563ee50a48eb6d3a92b8
MarioKusek <mario.kusek@fer.hr>
2019-03-23 13:47:25 +0100

    Players can play multiple moves.

--- src/test/java/hr/fer/tictactoe/domain/TicTacToeTest.java
+++ src/test/java/hr/fer/tictactoe/domain/TicTacToeTest.java
@@ -32,4 +32,19 @@ public class TicTacToeTest {
 		assertThat(game.getFieldValue("a1")).isEqualTo("o");
 		assertThat(game.getFieldValue("b1")).isEqualTo("x");
 	}
+
+	@Test
+	void enterPositionsForMultipleMoves() throws Exception {
+		TicTacToe game = new TicTacToe();
+		
+		game.play("a1");
+		game.play("b1");
+		game.play("a2");
+		game.play("b3");
+		
+		assertThat(game.getFieldValue("a1")).isEqualTo("o");
+		assertThat(game.getFieldValue("b1")).isEqualTo("x");
+		assertThat(game.getFieldValue("a2")).isEqualTo("o");
+		assertThat(game.getFieldValue("b3")).isEqualTo("x");
+	}
 }

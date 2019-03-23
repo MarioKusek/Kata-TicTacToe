2e1909533db1fab9795af6546eaff97be5b46abd
MarioKusek <mario.kusek@fer.hr>
2019-03-23 13:32:42 +0100

    We can get fake value in position.

---
+++ src/main/java/hr/fer/tictactoe/domain/TicTacToe.java
@@ -0,0 +1,8 @@
+package hr.fer.tictactoe.domain;
+
+public class TicTacToe {
+
+	public String getFieldValue(String position) {
+		return "o";
+	}
+}
---
+++ src/test/java/hr/fer/tictactoe/domain/TicTacToeTest.java
@@ -0,0 +1,15 @@
+package hr.fer.tictactoe.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class TicTacToeTest {
+
+	@Test
+	void emptyFieldPositionInNewGame() throws Exception {
+		TicTacToe game = new TicTacToe();
+		
+		assertThat(game.getFieldValue("a1")).isEqualTo("o");
+	}
+}

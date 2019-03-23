452e90320d244dd64f6e49c45dbf213a8a8f837e
MarioKusek <mario.kusek@fer.hr>
2019-03-23 20:24:31 +0100

    Integrated everything in main

---
+++ src/main/java/hr/fer/tictactoe/console/Main.java
@@ -0,0 +1,25 @@
+package hr.fer.tictactoe.console;
+
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import hr.fer.tictactoe.domain.TicTacToe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe(
+				new ConsoleView(new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8))));
+		
+		game.start();
+		try(Scanner scanner = new Scanner(System.in)) {
+			while(!game.isFinishedGame()) {
+				System.out.println("Enter your move:");
+				String line = scanner.nextLine();
+				game.play(line);
+			}
+		}
+	}
+}

7e8ef45b8a2bea349b9b6c233b3a268f14bd9f4b
MarioKusek <mario.kusek@fer.hr>
2019-03-28 15:06:29 +0000

    Extracted duplication to setup method.

--- src/test/java/hr/fer/tictactoe/console/ConsoleViewTest.java
+++ src/test/java/hr/fer/tictactoe/console/ConsoleViewTest.java
@@ -5,16 +5,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import hr.fer.tictactoe.domain.TicTacToe;
 
 public class ConsoleViewTest {
+	private StringWriter outBuffer;
+	private ConsoleView view;
+	
+	@BeforeEach
+	void setupOutputBuffer() {
+		outBuffer = new StringWriter();		
+		view = new ConsoleView(new PrintWriter(outBuffer));
+	}
+
 	@Test
 	void displayInvalidPosition() throws Exception {
-		StringWriter outBuffer = new StringWriter();
-		ConsoleView view = new ConsoleView(new PrintWriter(outBuffer));
-		
 		view.displayInvalidPosition("a6");
 		
 		assertThat(outBuffer.toString()).isEqualTo("Wrong position 'a6'. You can enter values from 'a1' to 'c3'\n");
@@ -22,9 +29,6 @@ public class ConsoleViewTest {
 
 	@Test
 	void displayPlayerTurn() throws Exception {
-		StringWriter outBuffer = new StringWriter();
-		ConsoleView view = new ConsoleView(new PrintWriter(outBuffer));
-		
 		view.displayPlayerTurn("x");
 		
 		assertThat(outBuffer.toString()).isEqualTo("Next player is 'x'.\n");
@@ -32,9 +36,6 @@ public class ConsoleViewTest {
 
 	@Test
 	void displayBoard() throws Exception {
-		StringWriter outBuffer = new StringWriter();
-		ConsoleView view = new ConsoleView(new PrintWriter(outBuffer));
-		
 		view.displayBoard(new TicTacToe("oo xx   o", null));
 
 		assertThat(outBuffer.toString()).isEqualTo(
@@ -48,9 +49,6 @@ public class ConsoleViewTest {
 	
 	@Test
 	void displayWinner() throws Exception {
-		StringWriter outBuffer = new StringWriter();
-		ConsoleView view = new ConsoleView(new PrintWriter(outBuffer));
-		
 		view.displayWinner("o");
 
 		assertThat(outBuffer.toString()).isEqualTo("The winner is player 'o'!!!!\n");
@@ -58,9 +56,6 @@ public class ConsoleViewTest {
 
 	@Test
 	void displayEndOfGameWithNoWinner() throws Exception {
-		StringWriter outBuffer = new StringWriter();
-		ConsoleView view = new ConsoleView(new PrintWriter(outBuffer));
-		
 		view.displayEndOfGameWithNoWinner();
 		
 		assertThat(outBuffer.toString()).isEqualTo("End of game!!! No winners!!!\n");
@@ -68,11 +63,9 @@ public class ConsoleViewTest {
 
 	@Test
 	void displayPositionAlreadyTaken() throws Exception {
-		StringWriter outBuffer = new StringWriter();
-		ConsoleView view = new ConsoleView(new PrintWriter(outBuffer));
-		
 		view.displayPositionAlreadyTaken("a1");
 		
 		assertThat(outBuffer.toString()).isEqualTo("Position 'a1' is already taken!\n");
 	}
 }
+
